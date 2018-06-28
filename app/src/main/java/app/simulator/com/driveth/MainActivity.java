38e8c6e44d02e7225b973886a8a7d446a0d6d1f1
Bilal <bilalbaig31@gmail.com>
2018-06-28 11:51:17 +0500

    test

--- app/src/main/java/app/simulator/com/driveth/MainActivity.java
+++ app/src/main/java/app/simulator/com/driveth/MainActivity.java
@@ -6,9 +6,15 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+        protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void function()
+    {
+        //todo
+        
     }
 
     //reverting the change
